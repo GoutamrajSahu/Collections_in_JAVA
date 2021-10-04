44632abd2c08d30ff620b743b18fc36a75d2a0d3
GoutamrajSahu <sahugoutamraj@gmail.com>
2021-10-04 23:19:49 +0530

    Initial commit

---
+++ src/Stack_Demo.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+import java.util.Stack;
+public class Stack_Demo {
+    public static void main(String[] args){
+     Stack<String> animals = new Stack<>();
+
+     //push() method:-
+     animals.push("Lion");
+     animals.push("Dog");
+     animals.push("Horse");
+     animals.push("Cat");
+     System.out.println("After pushing or adding elements to animals stack using push() method: "+ animals);
+
+     //peek() method:-
+     System.out.println("Getting the top or last added element of the stack using peek() method: "+ animals.peek());
+
+     //pop() method:-
+     animals.pop();
+     System.out.println("After popping or removing the top element from stack using pop() method: "+ animals);
+    }
+}
